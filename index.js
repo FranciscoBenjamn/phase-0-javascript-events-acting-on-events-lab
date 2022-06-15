8dbefeba1f857fa5c53a1bae7ef3b77e0c0055ca
Franklin Benjamin <franciscovibes14@gmail.com>
2022-06-15 17:14:32 -0400

    commit tree 99928d662f6d528dc191a5b1abb09480b9cdddd9

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function moveDodgerLeft() {
+  var dodger = document.getElementById('dodger')
+  var left = dodger.style.left
+  left = parseInt(left)
+  dodger.style.left = left - 10 + 'px'
+}
+function moveDodgerRight() {
+  var dodger = document.getElementById('dodger')
+  var left = dodger.style.left
+  left = parseInt(left)
+  dodger.style.left = left + 10 + 'px'
+}
